c4ddbdb7623881a7c9986911495fbcb005f22973
Patrick Murphy <patrick@patrickmurphyphoto.com>
2015-10-24 12:15:12 -0700

    add session filter for group

--- server-libs/session.js
+++ server-libs/session.js
@@ -1,3 +1,7 @@
+var objFilter = require('object-filter');
+var isNumeric = function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
 var session = {
     sessions: {
             1:{
@@ -5,11 +9,25 @@ var session = {
                 group: 1,
                 owner: 1,
                 times: {start:'10/10/15 13:35:32',end:'10/10/15 15:35:32'}
+            },
+            2:{
+                id:2,
+                group: 2,
+                owner: 1,
+                times:{start:'10/11/15 13:35:32',end:'10/11/15 15:35:32'}
             }
         },
     getList: function(req, res){
         res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(session.sessions));
+
+       if(req.query.hasOwnProperty('group') && isNumeric(req.query.group)){
+           var response = objFilter(session.sessions, function(v,k){
+               return v.group == req.query.group;
+           });
+           res.send(response);
+       }else{
+        res.send(session.sessions);
+       }
     },
     get: function(req, res){
         res.setHeader('Content-Type', 'application/json');
